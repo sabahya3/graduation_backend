9a7417471666ee195ba625950c343cbdb4b948c2
sabahya3 <sabahya3@example.com>
2023-02-05 02:56:30 +0200

    downgrade node version

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -10,7 +10,7 @@ const dbOptions = {
 function connectMongoose(callback) {
 
     const DB_KEY='mongodb://AS:RQ6IcBvQMygQiyAr@cluster0-shard-00-00.4lxon.mongodb.net:27017,cluster0-shard-00-01.4lxon.mongodb.net:27017,cluster0-shard-00-02.4lxon.mongodb.net:27017/GraduationProject?ssl=true&replicaSet=atlas-4yywzn-shard-0&authSource=admin&retryWrites=true&w=majority'
-
+    mongoose.set('strictQuery', true);
     mongoose.connect(DB_KEY, dbOptions, (err) => {
         if (err) console.log(err)
         else {
