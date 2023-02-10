0d3be652b09f4c2072e847962f065d17c19b68ec
sabahya3 <sabahya3@example.com>
2023-02-10 21:55:18 +0200

    credentials

--- app.js
+++ app.js
@@ -5,29 +5,31 @@ const cors = require('cors')
 require("dotenv").config();
 
 
-const clusterServer=require('./config/cluster_server')
-const adminRouter=require('./routes/admin');
-const subjectRouter=require('./routes/subject');
-const teacherRouter=require('./routes/teacher');
-const gradeRouter=require('./routes/grade');
+const clusterServer = require('./config/cluster_server')
+const adminRouter = require('./routes/admin');
+const subjectRouter = require('./routes/subject');
+const teacherRouter = require('./routes/teacher');
+const gradeRouter = require('./routes/grade');
 const isAuthorized = require('./middlewares/validation');
 
 
 const app = express()
-app.use(cors())
+app.use(cors(
+    {credentials: true }
+))
 app.use(cookieParser())
 app.use(express.json())
 
 // client - middleware > se4rver 
 
 // routes 
-app.use('/v1/admin',adminRouter)
-app.use('/v1/subject',isAuthorized , subjectRouter)
-app.use('/v1/teacher',isAuthorized , teacherRouter)
-app.use('/v1/grade',isAuthorized , gradeRouter)
+app.use('/v1/admin', adminRouter)
+app.use('/v1/subject', isAuthorized, subjectRouter)
+app.use('/v1/teacher', isAuthorized, teacherRouter)
+app.use('/v1/grade', isAuthorized, gradeRouter)
 
-app.get('/test', (req , res)=>{
-    res.json({success:'its now workin like a charm'})
+app.get('/test', (req, res) => {
+    res.json({ success: 'its now workin like a charm' })
 })
 
 // port number
