1d1dd0afb883f3716cc5343e39e934744f2ed062
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-04-08 00:31:31 +0200

    student routes

--- routes/student.js
+++ routes/student.js
@@ -4,7 +4,7 @@ const studentCtrl=require('../controllers/student')
 
 router.post('/addStudent',studentCtrl.addStudent)
 router.patch('/updateStudent/:id',studentCtrl.updateStudent)
-router.delete('/deleteStudent/:id',studentCtrl.updateStudent)
+router.delete('/deleteStudent/:id',studentCtrl.deleteStudent)
 router.get('/getStudentById/:id',studentCtrl.getStudentById)
 router.get('/getClassStudents/:classId',studentCtrl.getClassStudents)
 router.get('/getGradeStudents/:gradeId',studentCtrl.getGradeStudents)
