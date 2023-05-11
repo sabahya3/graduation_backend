19f5e43ab32417497b879cb106b243367f67c5d3
Ahmed El Sabahy <ahmedelsabahy@Ahmeds-MacBook-Pro.local>
2023-05-11 07:12:53 -0700

    scan qr1

--- routes/mobile/security.js
+++ routes/mobile/security.js
@@ -4,6 +4,8 @@ const securityCtrl=require('../../controllers/mobile/security')
 
 router.post('/takeAttendance', securityCtrl.takeAttendance)
 router.post('/getStudentNotificationsById', securityCtrl.getStudentAttendanceNotificationsWithPagination)
+router.post('/login', securityCtrl.login)
+
 
 
 
