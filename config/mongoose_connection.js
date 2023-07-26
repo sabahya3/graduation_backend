c648dc7bbb6adb7c274086543215aeb391c602e8
Ahmed El Sabahy <81205955+sabahya3@users.noreply.github.com>
2023-07-26 06:38:16 -0700

    Update mongoose_connection.js

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -9,9 +9,9 @@ const dbOptions = {
 
 function connectMongoose(callback) {
 
-    const DB_KEY='mongodb://AS:RQ6IcBvQMygQiyAr@cluster0-shard-00-00.4lxon.mongodb.net:27017,cluster0-shard-00-01.4lxon.mongodb.net:27017,cluster0-shard-00-02.4lxon.mongodb.net:27017/GraduationProject?ssl=true&replicaSet=atlas-4yywzn-shard-0&authSource=admin&retryWrites=true&w=majority'
+  
     mongoose.set('strictQuery', true);
-    mongoose.connect(DB_KEY, dbOptions, (err) => {
+    mongoose.connect(process.env.DB_KEY, dbOptions, (err) => {
         if (err) console.log(err)
         else {
             console.log('Db connected sucessfully!')
